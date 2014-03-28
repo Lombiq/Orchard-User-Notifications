9c0a383f52d9ba2305bde03e4559ecfddaa1e778
barthamark <none@none>
2014-03-28 17:57:40 +0100

    Quick implementation of NotificationBatch list. No other functions. RLTY-3

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Orchard;
+using Orchard.ContentManagement;
+using Orchard.Core.Common.Models;
+using Orchard.Localization;
+using Orchard.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +11,44 @@ using System.Web.Mvc;
 
 namespace RealtyShares.UserNotifications.Controllers
 {
-    // Implement admin listings here if no better idea.
     public class AdminController : Controller
     {
+        private readonly IContentManager _contentManager;
+        private readonly IOrchardServices _orchardServices;
+
+
+        public Localizer T { get; set; }
+
+        public ILogger Logger { get; set; }
+
+
+        public AdminController(IOrchardServices orchardServices)
+        {
+            _orchardServices = orchardServices;
+            _contentManager = orchardServices.ContentManager;
+
+            T = NullLocalizer.Instance;
+            Logger = NullLogger.Instance;
+        }
+
+
         public ActionResult Index()
         {
-            throw new NotImplementedException();
+            if (!_orchardServices.Authorizer.Authorize(Permissions.ManageNotifications))
+            {
+                return new HttpUnauthorizedResult(T("You are not allowed to manage notifications.").Text);
+            }
+
+            var notificationItems = _contentManager
+                .Query(VersionOptions.Published, Constants.NotificationBatchContentType)
+                .OrderByDescending<CommonPartRecord>(record => record.Id)
+                .List();
+
+            var notificationBatchShapes = _contentManager.GetShapesFactory(notificationItems, "SummaryAdmin");
+
+            var viewModel = _orchardServices.New.ViewModel(NotificationBatchShapes: notificationBatchShapes);
+
+            return View(viewModel);
         }
     }
 }
---
+++ Extensions/ContentManagerExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Orchard.ContentManagement
+{
+    internal static class ContentManagerExtensions
+    {
+        public static Func<IEnumerable<dynamic>> GetShapesFactory(this IContentManager contentManager, IEnumerable<ContentItem> contentItems, string displayType = "", string groupId = "")
+        {
+            return () => contentItems.Select(item => contentManager.BuildDisplay(item, displayType, groupId));
+        }
+    }
+}
