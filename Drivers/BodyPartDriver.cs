d62c621f1458eb07a82ed1bfc1ad9c612c355f6d
Márk Bartha <barthamark@gmail.com>
2014-04-04 17:02:48 +0200

    Overriding body and title part drivers and templates to display token helper.

---
+++ Drivers/BodyPartDriver.cs
@@ -0,0 +1,31 @@
+﻿using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Core.Common.Models;
+using Orchard.Core.Title.Models;
+using Orchard.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RealtyShares.UserNotifications.Drivers
+{
+    public class BodyPartDriver : ContentPartDriver<BodyPart>
+    {
+        protected override string Prefix { get { return "Body"; } }
+
+
+        protected override DriverResult Editor(BodyPart part, dynamic shapeHelper)
+        {
+            return ContentShape("Parts_Common_Body_Edit_Tokenized", () => 
+                shapeHelper.EditorTemplate(TemplateName: "Parts.Common.Body.Tokenized", Model: part, Prefix: Prefix));
+        }
+
+        protected override DriverResult Editor(BodyPart part, IUpdateModel updater, dynamic shapeHelper)
+        {
+            updater.TryUpdateModel(part, Prefix, null, null);
+
+            return Editor(part, shapeHelper);
+        }
+    }
+}
---
+++ Drivers/TitlePartDriver.cs
@@ -0,0 +1,30 @@
+﻿using Orchard.ContentManagement;
+using Orchard.ContentManagement.Drivers;
+using Orchard.Core.Title.Models;
+using Orchard.Localization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RealtyShares.UserNotifications.Drivers
+{
+    public class TitlePartDriver : ContentPartDriver<TitlePart>
+    {
+        protected override string Prefix { get { return "Title"; } }
+
+
+        protected override DriverResult Editor(TitlePart part, dynamic shapeHelper)
+        {
+            return ContentShape("Parts_Title_Edit_Tokenized", () => 
+                shapeHelper.EditorTemplate(TemplateName: "Parts.Title.Tokenized", Model: part, Prefix: Prefix));
+        }
+
+        protected override DriverResult Editor(TitlePart part, IUpdateModel updater, dynamic shapeHelper)
+        {
+            updater.TryUpdateModel(part, Prefix, null, null);
+
+            return Editor(part, shapeHelper);
+        }
+    }
+}
