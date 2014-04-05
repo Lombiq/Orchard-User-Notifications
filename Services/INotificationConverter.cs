a4a5447ae6988bb1ee30bff51fb083f4c82ac762
Zoltán Lehóczky <lehoczky_zoltan@pyrocenter.hu>
2014-04-05 19:41:52 +0200

    Adding method documentation for INotificationConverter

--- Services/INotificationConverter.cs
+++ Services/INotificationConverter.cs
@@ -14,6 +14,12 @@ namespace RealtyShares.UserNotifications.Services
     /// </summary>
     public interface INotificationConverter : IDependency
     {
+        /// <summary>
+        /// Converts a notification batch content item to an INotification object.
+        /// </summary>
+        /// <param name="notificationBatch">The notification batch to convert.</param>
+        /// <param name="recipient">The user who is the recipient of the notification.</param>
+        /// <returns>The INotification object.</returns>
         INotification ConvertBatchToNotification(IContent notificationBatch, IUser recipient);
     }
 }
